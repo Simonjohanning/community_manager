e653c35284bcccdca328d0797571f6a9e444a264
Simon Johanning <Simonjohanning@gmail.com>
2017-03-30 14:42:31 +0200

    calendar and script tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+
+require 'fileutils'
+
 class DashboardController < ApplicationController
   def index
+    @users = User.all
   end
 end
